e81cf69d4394173e418910f4892cedc6c226d518
Leonard <leonard.horne@gmail.com>
2017-08-30 14:38:48 -0400

    Add validation to email (uniqueness/format) and before_validation method

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+
+
+  before_validation :downcase_email
+
+  def downcase_email
+    email.downcase if email
+  end
+
+end
